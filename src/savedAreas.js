e5b9a45b1312a7315a6911cd8ea42cae6c9811ac
Halli <163026985+halliann@users.noreply.github.com>
2024-10-02 12:52:30 -0500

    Update savedAreas.js

--- src/savedAreas.js
+++ src/savedAreas.js
@@ -1,16 +1,1 @@
 
-class SelectedAreas {
-    constructor() {
-        this.list = [];
-    }
-
-    addArea(save) {
-        const newLocation = new Location();
-
-        this.list.push(newLocation);
-    }
-
-    removeArea(unsave) {
-        this.list.pop
-    }
-};
